77a7019aee8be5cf6cf136579d0e8aca3c0cc20d
Jose Botello <j.botello@uniandes.edu.co>
2023-05-19 23:57:26 -0500

    Commit

--- src/app/partidos/partidos.module.ts
+++ src/app/partidos/partidos.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PartidosComponent } from './partidos.component';
+import { PartidoListComponent } from './partido_listar/partido_listar.component';
+import { Partidos_detail } from './partidos_detail';
+import { Partidos_detailComponent } from './partidos_detail/partidos_detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PartidosComponent]
+  exports: [PartidoListComponent],
+  declarations: [Partidos_detailComponent,PartidoListComponent]
 })
 export class PartidosModule { }
